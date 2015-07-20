e22b2c460cc768f5018a41198da7ac84d06dac14
ktarow <ktarou569@gmail.com>
2015-07-21 01:48:54 +0900

    split detail, check parameter. add news command

--- lib/ro69/commands/main.rb
+++ lib/ro69/commands/main.rb
@@ -8,8 +8,32 @@ module Ro69
         puts "#{::Ro69::VERSION}"
       end
 
-      desc "liverepo", "ro69 live report list top 20"
-      def liverepo(category = "all")
+      desc "report", "ro69 live report top 20"
+      def report(category = "all")
+        category_id = check_category category
+        uri = "#{::Ro69::BASE_URI}/live?category=#{category_id}"
+        selector = "h3.ttl_l > a"
+
+        detail_uri = get_detail_uri uri, selector
+
+        article = Sanitize.clean @agent.get(detail_uri).search("div.article_box_inner")
+        STDOUT.puts article
+      end
+
+      desc "news", "ro69 news top 20"
+      def news(category = "all")
+        category_id = check_category category
+        uri = "#{::Ro69::BASE_URI}/news?category_id=#{category_id}"
+        selector = "h3.ttl_l > a"
+
+        detail_uri = get_detail_uri uri, selector
+        
+        article = Sanitize.clean @agent.get(detail_uri).search("div.article_box_inner")
+        STDOUT.puts article
+      end
+
+      private
+      def check_category(category)
         begin 
           case category
           when "all"
@@ -21,31 +45,28 @@ module Ro69
           else
             raise LiveRepoError, "option => (all|japan|foreign)"
           end
+          category_id
         rescue LiveRepoError => ex
           STDERR.puts ex.message
           exit 1
         end
+      end
 
-        query_string = "category=#{category_id}"
-        url = "#{::Ro69::BASE_URI}/live?category=#{category_id}"
-
+      def get_detail_uri(uri, selector)
         storage = {}
-        @agent.get(url).search("h3.ttl_l > a").each_with_index do |item, num|
+        @agent.get(uri).search(selector).each_with_index do |item, num|
           title = item.attribute("title").value
           href = ::Ro69::BASE_URI + item.attribute("href").value
-
           storage.store(num, {title: title, href: href})
         end
 
         target = storage.each_with_object([]) do |(num, object), array|
-        array << num.to_s + DELIMITER + object[:title]
+          array << num.to_s + DELIMITER + object[:title]
         end
 
         select_num = Ifilter.filtering(target).split(DELIMITER).first.to_i
-        select_url = storage[select_num][:href]
-
-        article = Sanitize.clean @agent.get(select_url).search("div.article_box_inner")
-        STDOUT.puts article
+        detail_uri = storage[select_num][:href]
+        detail_uri
       end
     end
   end
